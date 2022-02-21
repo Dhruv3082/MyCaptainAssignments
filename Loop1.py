eb58a4954fc9bdc0fa3631d781cdd2e8b939889b
Patel Dhruvkumar Dilipbhai <78692234+Dhruv3082@users.noreply.github.com>
2022-02-21 18:06:29 +0530

    Created by Dhruv Patel

---
+++ Loop1.py
@@ -0,0 +1,15 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+j = 0
+i = 0
+while i < 5:
+    if list1[i]>0:
+        print(list1[i])
+    i += 1
+
+print("The Work for first list has been done!")
+
+for number in list2:
+    if number>0:
+        print(number)
+    
